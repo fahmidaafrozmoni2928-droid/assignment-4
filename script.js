7d4595154cc1cb6da2f4bcc33988d1c9e8e45a31
fahmidaafrozmoni2928-droid <fahmidaafrozmoni2928@gmail.com>
2026-02-25 22:49:10 +0600

    toggle button

--- script.js
+++ script.js
@@ -20,13 +20,75 @@ function toggleStyle(id){
 
 }   
 
-const btnInterview = document.getElementById('INTERVIEW-btn');
-const btnRejected = document.getElementById('REJECTED-btn');
-const notApplied = document.getElementById('not-applied');
-btnInterview.addEventListener('click', function(){
-    
-       notApplied.innerText = btnInterview;
-      
-      console.log(btnInterview);
+const btnInterview = document.querySelector('.INTERVIEW-btn');
+const btnRejected = document.querySelector('.REJECTED-btn');
+const notApplied = document.querySelector('.not-applied');
+const card = document.querySelector('.card');
+const interviewCount = document.getElementById('interview-count');
+const rejectCount = document.getElementById('rejected-count');
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('INTERVIEW-btn')){
+      const card =  e.target.closest('.card');
+      const notApplied = card.querySelector('.not-applied');
+
+        notApplied.innerText = btnInterview.innerText;
+
+       notApplied.classList.remove('bg-[#F8FAFC]', 'py-3', 'px-2');
+            
+        
+            notApplied.classList.add('text-green-800', 'border-2', 'border-green-800' );
+
+      interviewCount.innerText++;
+      e.target.disabled = true;
+
+
+         //  console.log(event);
+
+    }
+
+       
+   if(e.target.classList.contains('REJECTED-btn')){
+      const card =  e.target.closest('.card');
+      const notApplied = card.querySelector('.not-applied');
+
+        notApplied.innerText = btnRejected.innerText;
+
+       notApplied.classList.remove('bg-[#F8FAFC]', 'py-3', 'px-2');
+            
+        
+            notApplied.classList.add('text-red-800', 'border-2', 'border-red-800' );
+
+      rejectCount.innerText++;
+      e.target.disabled = true;
+
+
+         //  console.log(event);
+
+    }  
     
 })
+
+
+const png = document.getElementById('png');
+const allCards = document.getElementById('all-cards')
+document.addEventListener('click', function(e){
+    if(e.target.classList.contains('interview-filter-btn')){
+    allCards.classList.add('hidden');
+   png.classList.remove('hidden');
+    }
+
+    if(e.target.classList.contains('reject-filter-btn')){
+        allCards.classList.add('hidden');
+       png.classList.remove('hidden');
+        }
+        
+        
+        if(e.target.classList.contains('all-filter-btn')){
+            allCards.classList.remove('hidden');
+           png.classList.add('hidden');
+            }        
+})
+        
+
+
+
